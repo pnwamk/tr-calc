e0f9ff2826750d63fa2843b7cbbfa9341f79da5a
Andrew Kent <sgtamk@gmail.com>
2014-04-24 11:28:11 -0400

    Basic defs complete

--- LambdaTR.v
+++ LambdaTR.v
@@ -7,18 +7,34 @@ Open Scope list_scope.
 
 Section LTR.
 
+Inductive id : Type :=
+  Id : nat -> id.
+
+(* Objects *)
+Inductive object : Type :=
+| o_nil : object
+| o_var  : id -> object. 
+
+(* Types *)
 Inductive type : Type :=
 | t_super : type
 | t_num   : type
 | t_true  : type
 | t_false : type
 | t_union : list type -> type
-| t_fun   : type -> type -> type. 
-(* TODO:  How/where do the latent propositions and objects of a function type fit?? *)
+| t_fun   : id -> type -> prop -> prop -> object -> type -> type
 
-Definition t_bool := (t_union (t_true :: t_false :: nil)).
-Definition t_bottom := (t_union nil).
+(* Propositions *)
+with prop : Type :=
+| p_type  : type -> id -> prop
+| p_neg   : type -> id -> prop
+| p_impl  : prop -> prop -> prop
+| p_disj  : prop -> prop -> prop
+| p_conj  : prop -> prop -> prop
+| p_false : prop
+| p_true  : prop.
 
+(* Primitive Operations *)
 Inductive primop : Type :=
 | add1   : primop
 | iszero : primop
@@ -26,19 +42,24 @@ Inductive primop : Type :=
 | isbool : primop
 | isproc : primop.
 
-Inductive id : Type :=
-  Id : nat -> id.
-
+(* Expressions *)
 Inductive expr : Type :=
-| e_num    : nat -> expr
-| e_false  : expr
-| e_true   : expr
-| e_primop : primop -> expr
-| e_if     : expr -> expr -> expr -> expr
-| e_lam    : id -> type -> expr -> expr
+| e_var     : id -> expr
 | e_app    : expr -> expr -> expr
-| e_id     : id -> expr.
+| e_abs    : id -> type -> expr -> expr
+| e_if     : expr -> expr -> expr -> expr
+| e_primop : primop -> expr
+| e_true   : expr
+| e_false  : expr
+| e_num    : nat -> expr.
+
+(* Common Type Abbreviations *)
+Definition t_bool := (t_union (t_true :: t_false :: nil)).
+Definition t_bottom := (t_union nil).
 
 (* TODO: Do we need Integers to represent numbers? Reals? *)
+
+(* Environments *)
+Definition Env := list prop.
 
 End LTR.
